73b17f5e25d859da455a82a4b5d15e8cdb3b79c6
kuntalmaity <kuntal.1461@gmail.com>
2024-08-15 23:05:13 +0530

    by kuntal : changes the of the page redicrection

--- buskart-customer-service/customer-service-core/src/main/buskart_customer_service_main.go
+++ buskart-customer-service/customer-service-core/src/main/buskart_customer_service_main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+
+
+
+
+
+func main()  {
+	// define the directory where the satic  file is presnt 
+
+	//staticDir :="./buskart-customer-service/customer-service-frontend/public"
+
+	staticDir := filepath.Join("..", "..", "..", "customer-service-frontend", "public")
+
+	// Check if the directory exists
+	if _, err := os.Stat(staticDir); os.IsNotExist(err) {
+		log.Fatalf("Static directory does not exist: %s", staticDir)
+	}
+
+	// serve the satic file 
+
+	http.Handle("/",http.FileServer(http.Dir(staticDir)))
+
+	// get the port from the env of defalut 8080
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port="8080"	
+	}
+
+	log.Printf("server is running on the http://localhost:%s/ ",port)
+	err := http.ListenAndServe(":"+port , nil)
+
+	if err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
